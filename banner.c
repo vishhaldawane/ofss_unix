4c65e32f935407a99f70be107eae99cb0a570708
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2025-05-20 12:59:48 +0530

    updated

--- banner.c
+++ banner.c
@@ -8,11 +8,17 @@
 	Team 	    : Alpha	
 	Version	    : 1.0
 */
-
-
 #include <stdio.h>
+int far() {
+  printf("\nthis is a far function\n");
+  return 0;
+}
 int main()
 {
   printf("\n\n --- B A N N E R  I S  P R I N T E D --- \n\n");
+  return 0;
+}
+int foo() {
+  printf("\nthis is a foo function\n");
   return 0;
 }
